35a47a2431839b78394d112f96d9c384c30b1fc8
Wighawag <wighawag@gmail.com>
2019-05-17 16:22:10 +0100

    ERC1155: use isERC1155TokenReceiver

--- src/Interfaces/ERC1155.sol
+++ src/Interfaces/ERC1155.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.5.2;
 
 interface ERC1155TokenReceiver {
-   function expectERC1155() external pure returns(bytes4);
+   function isERC1155TokenReceiver() external view returns (bytes4);
    function onERC1155Received(address _operator, address _from, uint256 _id, uint256 _value, bytes calldata _data) external returns(bytes4); 
    function onERC1155BatchReceived(address _operator, address _from, uint256[] calldata _ids, uint256[] calldata _values, bytes calldata _data) external returns(bytes4);
 }
