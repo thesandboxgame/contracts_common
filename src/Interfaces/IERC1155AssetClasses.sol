a5a298f7f4740ebea81f273cac55ccd1748d8c95
Nicole Zhang <nicolez@animocabrands.com>
2019-09-04 22:39:09 +0800

    - remove ERC20MetaTxExtension

--- src/Interfaces/IERC1155AssetClasses.sol
+++ src/Interfaces/IERC1155AssetClasses.sol
@@ -1,7 +1,25 @@
 pragma solidity ^0.5.0;
 
 interface IERC1155AssetClasses {
+
+    /**
+        @dev Tells whether a id is fungible class
+        @param id The ID to query
+        @return bool whether the given id is fungible class
+     */
     function isFungible(uint256 id) external view returns(bool);
+
+    /**
+        @dev Tells whether a id is non-fungible class
+        @param id The ID to query
+        @return bool whether the given id is non-fungible class
+     */
     function isNonFungibleClass(uint256 id) external view returns(bool);
+
+    /**
+        @dev Tells whether a id is non-fungible token
+        @param id The ID to query
+        @return bool whether the given id is non-fungible token
+     */
     function isNonFungibleItem(uint256 id) external view returns(bool);
 }
