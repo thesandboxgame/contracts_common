246b773d1045e657cad3ca661741302b07485d1d
Wighawag <wighawag@gmail.com>
2019-09-20 11:26:40 +0100

    fix warning

--- src/BaseWithStorage/ProxyImplementation.sol
+++ src/BaseWithStorage/ProxyImplementation.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.5.2;
 contract ProxyImplementation {
     mapping(string => bool) _initialised;
 
-    modifier phase(string memory phase) {
-        if (!_initialised[phase]) {
-            _initialised[phase] = true;
+    modifier phase(string memory phaseName) {
+        if (!_initialised[phaseName]) {
+            _initialised[phaseName] = true;
             _;
         }
     }
