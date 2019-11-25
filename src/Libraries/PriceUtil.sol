39b0382e0968c7059f1c060e3c820a1d597cf217
Wighawag <wighawag@gmail.com>
2019-11-25 10:36:37 +0000

    PriceUtil : use SafeMath

--- src/Libraries/PriceUtil.sol
+++ src/Libraries/PriceUtil.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.5.2;
 
+import "./SafeMathWithRequire.sol";
+
 library PriceUtil {
+    using SafeMathWithRequire for uint256;
+
     function calculateCurrentPrice(
         uint256 startingPrice,
         uint256 endingPrice,
@@ -10,10 +14,17 @@ library PriceUtil {
         if (secondsPassed > duration) {
             return endingPrice;
         }
-        int256 totalPriceChange = int256(endingPrice) - int256(startingPrice);
-        int256 currentPriceChange = (totalPriceChange * int256(secondsPassed)) / int256(duration);
-
-        return uint256(int256(startingPrice) + currentPriceChange);
+        if (endingPrice == startingPrice) {
+            return endingPrice;
+        } else if (endingPrice > startingPrice) {
+            return startingPrice.add(
+                (endingPrice.sub(startingPrice)).mul(secondsPassed).div(duration)
+            );
+        } else {
+            return startingPrice.sub(
+                (startingPrice.sub(endingPrice)).mul(secondsPassed).div(duration)
+            );
+        }
     }
 
     function calculateFee(uint256 price, uint256 fee10000th)
@@ -22,7 +33,7 @@ library PriceUtil {
         returns (uint256)
     {
         // _fee < 10000, so the result will be <= price
-        return (price * fee10000th) / 10000;
+        return (price.mul(fee10000th)) / 10000;
     }
 
 }
