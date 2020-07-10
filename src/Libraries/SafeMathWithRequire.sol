ef303492bfbae7c44ab7ce08374b0458875b06da
Wighawag <wighawag@gmail.com>
2020-07-10 15:05:20 +0100

    use != 0 for efficiency

--- src/Libraries/SafeMathWithRequire.sol
+++ src/Libraries/SafeMathWithRequire.sol
@@ -26,7 +26,7 @@ library SafeMathWithRequire {
      * @dev Integer division of two numbers, truncating the quotient.
      */
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b > 0, "divbyzero");
+        require(b != 0, "divbyzero");
         // uint256 c = a / b;
         // assert(a == b * c + a % b); // There is no case in which this doesn't hold
         return a / b;
